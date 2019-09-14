e7fd53b2e1cc0b9cbee95609d44951c421a7f923
tranthihuyen0903 <huyentran090315@gmail.com>
2019-09-14 09:59:17 +0700

    cap nhat them

--- Tuan1/src/lab1/lab1bai4.java
+++ Tuan1/src/lab1/lab1bai4.java
@@ -6,8 +6,11 @@ public class lab1bai4 {
     public static void main(String[] args) {
         double a, b, c;
         Scanner sc= new Scanner(System.in);
+        System.out.println("Nhap so a:");
         a= sc.nextInt();
+        System.out.println("Nhap so b:");
         b= sc.nextInt();
+        System.out.println("Nhap so c:");
         c= sc.nextInt();
         double delTa= (b*b)-4*a*c;
         double canDelTa= Math.sqrt(delTa);
